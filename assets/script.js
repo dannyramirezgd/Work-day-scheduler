f55de89756c9d88a7d46ceaa597067390c269884
Danny Ramirez <danny.ramirezgd@gmail.com>
2021-12-15 10:33:13 -0500

    added some css styles, added the hours and the general skeleton

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+//display current day at top of calendar
+$("#currentDay").text(moment().format('MMM Do, YYYY'));
+//have time blocks only exist during office hours
+
+//make time blocks color coded red if past or yellow if approaching
+
+//make time blocks editable by clicking into them
+
+//when blocks are saved or edited save to local storage
+
+//load local storage when page loads
