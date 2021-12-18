8ec9a5c64d793ff29576ed45b57be02dbdf4b141
dannyrgd <danny.ramirezgd@gmail.com>
2021-12-17 20:34:12 -0500

    full schedule functionality

--- assets/script.js
+++ assets/script.js
@@ -1,134 +1,120 @@
-//store all daily events into object
+//store all info for each hour row as an array of objects
 events = [
     {
-        index: "0",
+        //to create class based on time of day
         militaryHour: "09",
+        //to give row each label
         time: "09",
+        //give each row context for am or pm
         ext: "am",
+        //event text that user will input
         text: "",
     },
     {
-        index: "1",
         militaryHour: "10",
         time: "10",
         ext: "am",
         text: "",
     },
     {
-        index:"2",
         militaryHour:"11",
         time:"11",
         ext:"am",
         text:""
     },
     {
-        index:"3",
         militaryHour:"12",
         time:"12",
         ext:"pm",
         text:"",
     },
     {
-        index:"4",
         militaryHour:"13",
         time:"01",
         ext:"pm",
         text:"",
     },
     {
-        index:"5",
         militaryHour:"14",
         time:"02",
         ext:"pm",
         text:"",
     },
     {
-        index:"6",
         militaryHour:"15",
         time:"03",
         ext:"pm",
         text:"",
     },
     {
-        index: "7",
         militaryHour: "16",
         time:"04",
         ext:"pm",
         text:"",
     },
     {
-        index:"8",
         militaryHour:"17",
         time:"05",
         ext:"pm",
         text:"",
     }
 ];
-console.log(events);
 //display current day at top of calendar
 $("#currentDay").text(moment().format('MMM Do, YYYY'));
 
 //when blocks are saved or edited save to local storage
 var savePlanner = function(){
-    localStorage.setItem("events", JSON.stringify(events))
-}
-
-//load local storage when page loads
-var loadEvents = function(){
-    events = JSON.parse(localStorage.getItem("events"));   
-}
-function auditDay (){
-    var currentDay = JSON.parse(localStorage.getItem("events"));
-
-
-    //savePlanner();
-    loadEvents();
+    localStorage.setItem("today", JSON.stringify(events))
 };
-//load all rows with time, forms, and buttons
-$.each(events, function(i, val){
-    //create entire hour row
-    //I can't create an ID of the current Index of the objects events[i].index
+
+//load local storage but only for the current day
+var loadEvents = function(){
+    //when the clock hits 23:59, the local storage clears, otherwise it will display what it has
+    if (moment().format("HH:mm") <= "23:58"){
+        events = JSON.parse(localStorage.getItem("today"))
+    }else{
+        localStorage.clear();
+    };   
+};
+//page load all rows with time, text areas, and buttons, along with any previous event data and classes
+$.each(events, function(i){
+    loadEvents();
+    //create entire hour event row and append it to the container
     var currentHour = $("<form>").attr({"class":"row justify-content-around"})
-
     $(".container").append(currentHour)
-    //create hour section
+    //create section to indicate the label for each hour
     var currentTime = $("<div>").text(events[i].time +" " + events[i].ext).attr({"class":"col-2 hour"});
-    //create event text area
+    //create event text area for each hour
     var currentEvent = $("<div>").attr({"class":"col-9 description p-0"});
-    var currentEventData = $("<textarea>").attr({"class":"event-text col-12 h-100"});
-    currentEventData.attr("id", events.index);
+    var currentEventData = $("<textarea>").attr({"class":"event-text col-12 h-100"}).val(events[i].text);
     currentEvent.append(currentEventData);
-
+    //check the time and color the row accordingly
     if (events[i].militaryHour < moment().format("HH")){
-        currentEventData.attr({"id":"houry"},{"class":"event-text past col-12 h-100",}).addClass(events[i].militaryHour)
+        currentEventData.addClass("past event-text col-12 h-100")
     }else if (events[i].militaryHour == moment().format("HH")){
-        currentEventData.attr({"id":"houry"},{"class":"event-text present col-12 h-100"}).addClass(events[i].militaryHour)
+        currentEventData.addClass("present event-text col-12 h-100")
     }else if (events[i].militaryHour > moment().format("HH")){
-        currentEventData.attr({"id":"houry"},{"class":"event-text future col-12 h-100"}).addClass(events[i].militaryHour)
+        currentEventData.addClass("future event-text col-12 h-100")
     }
+    //append both the hour label and text area
     currentHour.append(currentTime);
     currentHour.append(currentEvent);
     
     //create save buttons
-    var saveBtnIcon = $("<span>").attr({"class":"oi oi-calendar"});
-    var saveBtn = $("<button>").attr({"class":"col-1 saveBtn"});
+    var saveBtnIcon = $("<span>").addClass("oi oi-calendar");
+    var saveBtn = $("<button>").addClass("col-1 saveBtn");
     saveBtn.append(saveBtnIcon);
     currentHour.append(saveBtn);
-    
-    //create save button
-
 });
-//auditDay();
 
-//save data btn function
+//when triggered, text value within textarea is captured and saved based on the row and saved to localStorage
 $(".saveBtn").on("click", function(event){
     event.preventDefault();
     var eventText = $(this).parent().children().find(".event-text").val();
-    var eventData = $(this).parent().children().find("#houry")
+    var eventData = $(this).parent().children().find(".event-text")
     var eventIndex = $(this).parent().index()
     eventData.append(eventText);
     events[eventIndex].text = eventText
     savePlanner();
-}
-    //loadEvents();
-);
+});
+loadEvents();
